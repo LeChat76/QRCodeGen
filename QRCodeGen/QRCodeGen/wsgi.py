804c9d4d705d68ca5ddb0cc0432dd903f8fc3265
LeChat76 <cedric.delauney@gmail.com>
2025-02-07 16:21:17 +0100

    fix wsgi

--- QRCodeGen/QRCodeGen/wsgi.py
+++ QRCodeGen/QRCodeGen/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lechat76_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QRCodeGen.settings')
 
 application = get_wsgi_application()
