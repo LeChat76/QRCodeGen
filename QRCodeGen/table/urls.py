83aba6c385e57c6035e10964d86a4a1d58154854
LeChat76 <cedric.delauney@gmail.com>
2025-02-07 15:29:06 +0100

    ajout du tableau avec arg

--- QRCodeGen/table/urls.py
+++ QRCodeGen/table/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import accueil
+from .views import table_view
 
 urlpatterns = [
     path('', accueil, name='accueil'),
+    path('table/<str:col1>/<str:col2>/<str:col3>/<str:col4>/<str:col5>/', table_view, name='table'),
 ]
--- QRCodeGen/table/views.py
+++ QRCodeGen/table/views.py
@@ -2,5 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def accueil(request):
-    return HttpResponse("Bienvenue sur mon site Django simple !")
+    return HttpResponse("L'url est incomplet....")
+
+def table_view(request, col1, col2, col3, col4, col5):
+    context = {
+        'col1': col1,
+        'col2': col2,
+        'col3': col3,
+        'col4': col4,
+        'col5': col5,
+    }
+    return render(request, 'table/table.html', context)
+
 
