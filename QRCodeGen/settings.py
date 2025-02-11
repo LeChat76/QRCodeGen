6ca4b4fd40ce3060549c7a54eea142f0768a24ed
LeChat76 <cedric.delauney@gmail.com>
2025-02-11 17:23:57 +0100

    adaptation logo et tableau

--- QRCodeGen/settings.py
+++ QRCodeGen/settings.py
@@ -123,9 +123,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Chemins supplémentaires pour rechercher les fichiers statiques
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
