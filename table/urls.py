de966209bb500e9b73bce2912b4b112d7adcdd41
LeChat76 <cedric.delauney@gmail.com>
2025-02-13 11:41:45 +0100

    ajout colonne etage 3

--- table/urls.py
+++ table/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import accueil
-from .views import table_view
+from .views import table_view, accueil
 
 urlpatterns = [
     path('', accueil, name='accueil'),
-    path('table/<str:col1>/<str:col2>/<str:col3>/<str:col4>/<str:col5>/', table_view, name='table'),
+    path('table/<str:col1>/<str:col2>/<str:col3>/<str:col4>/<str:col5>/<str:col6>/', table_view, name='table_view'),
 ]
+
--- table/views.py
+++ table/views.py
@@ -2,16 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def accueil(request):
-    return HttpResponse("L'url est incomplet....")
+    return HttpResponse("L'URL est incomplète...")
 
-def table_view(request, col1, col2, col3, col4, col5):
+def table_view(request, col1, col2, col3, col4, col5, col6):
+    # Séparer les valeurs de col4 et col5 en listes
+    etage_1_values = col4.split(";")
+    etage_2_values = col5.split(";")
+    etage_3_values = col6.split(";")
+
+    # Vérifier que chaque colonne contient bien 5 éléments (optionnel)
+    if len(etage_1_values) != 5 or len(etage_2_values) != 5:
+        return HttpResponse("Erreur : les colonnes 'Etage 1' et 'Etage 2' doivent contenir 5 valeurs séparées par ';'.")
+
+    # Contexte pour le template
     context = {
         'col1': col1,
         'col2': col2,
         'col3': col3,
-        'col4': col4,
-        'col5': col5,
+        'etage_1_values': etage_1_values,
+        'etage_2_values': etage_2_values,
+        'etage_3_values': etage_3_values,
     }
+    
     return render(request, 'table.html', context)
+
 
 
