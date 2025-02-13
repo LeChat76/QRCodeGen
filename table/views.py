ca65640ec02fcecb01b3db37f2513b73311e50a3
LeChat76 <cedric.delauney@gmail.com>
2025-02-13 15:49:43 +0100

    mise en forme tableau

--- table/views.py
+++ table/views.py
@@ -5,16 +5,14 @@ def accueil(request):
     return HttpResponse("L'URL est incomplète...")
 
 def table_view(request, col1, col2, col3, col4, col5, col6):
-    # Séparer les valeurs de col4 et col5 en listes
     etage_1_values = col4.split(";")
     etage_2_values = col5.split(";")
     etage_3_values = col6.split(";")
 
     # Vérifier que chaque colonne contient bien 5 éléments (optionnel)
-    if len(etage_1_values) != 5 or len(etage_2_values) != 5:
+    if len(etage_1_values) != 5 or len(etage_2_values) != 5 or len(etage_3_values) != 5:
         return HttpResponse("Erreur : les colonnes 'Etage 1' et 'Etage 2' doivent contenir 5 valeurs séparées par ';'.")
 
-    # Contexte pour le template
     context = {
         'col1': col1,
         'col2': col2,
